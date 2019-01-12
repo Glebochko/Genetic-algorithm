70019a81ec62d5f52bbb58c862898fe36bdc34b0
gleb skryabin <glebka.sk@gmail.com>
2019-01-12 14:07:36 +0300

    first bot

--- Gen_alg.py
+++ Gen_alg.py
@@ -23,11 +23,23 @@ class bot:
     def drawline(self, window, x1, y1, x2, y2):
         line = Line(Point(x1, y1), Point(x2, y2))
         line.setFill('blue')
+        line.setWidth(3)
         line.draw(window)
 
-    def darwbot(self, window, cellsize):
-        self.drawline(window, self.x * cellsize, self.y * cellsize, (self.x + 1) * cellsize, self.y * cellsize)
+    def drawbot(self, window, cellsize):
+        self.p1 = Point(self.x * cellsize, self.y * cellsize)
+        self.p2 = Point((self.x + 1) * cellsize, self.y * cellsize)
+        self.p3 = Point((self.x + 1) * cellsize, (self.y + 1) * cellsize)
+        self.p4 = Point(self.x * cellsize, (self.y + 1) * cellsize)
 
+        self.verticles = [self.p1, self.p2, self.p3, self.p4]
+
+        self.cell = Polygon(self.verticles)
+        self.cell.setFill('blue')
+        self.cell.setOutline('black')
+        self.cell.setWidth(1)
+
+        self.cell.draw(window)
 
     def __str__(self):
         print(self.DNA)
@@ -36,12 +48,16 @@ class bot:
 
 class gen_alg:
     def __init__(self):
+        self.mybots = []
         self.cellsize = 10
 
-    def createWindow(self, width, hight):
-        self.width = width
-        self.hight = hight
-        self.window = GraphWin('Game Of Live', self.width, self.hight)
+    def createWindow(self, xmax, ymax, cellsize):
+        self.cellsize = cellsize
+        self.xmax = xmax
+        self.ymax = ymax
+        self.width = self.xmax * self.cellsize
+        self.hight = self.ymax * self.cellsize
+        self.window = GraphWin('Game Of Life', self.width, self.hight)
     
     def drawline(self, x1, y1, x2, y2):
 
@@ -64,21 +80,21 @@ class gen_alg:
             self.drawline(0, y, self.width, y)
             y += self.cellsize
 
+    def newbot(self, x, y):
+        self.mybots.append(bot(x, y))
+
 
     def start(self):
-        self.drawCells()  
+        #self.drawCells() 
+        self.newbot(2, 5)
+        self.mybots[0].drawbot(self.window, self.cellsize) 
         self.pause()
     
 
 def main():
     gol = gen_alg()
-    gol.createWindow(800, 600)
-    gol.cellsize = 20
+    gol.createWindow(40, 30, 20)
     gol.start()
-
-    #newbot = bot()
-
-    #print(newbot)
 
 
 
